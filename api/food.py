66f1de032b6235441d11aa56fce6de34a6374158
ad1tyad3sa1 <aditya7desai@gmail.com>
2024-03-22 11:08:49 -0700

    food api

--- api/food.py
+++ api/food.py
@@ -1,59 +1,45 @@
-from flask import Flask, request, jsonify
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
-from flask_restful import Resource
-from flask import Blueprint, jsonify, request 
+from flask import Flask, request, jsonify, Blueprint
 from flask_restful import Api, Resource
-
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
 food_api = Blueprint('food_api', __name__, url_prefix='/api/food')
 api = Api(food_api)
 
-class FoodAPI(Resource):
+class FoodPredictor(Resource):
     def __init__(self):
         self.data = pd.read_csv('filtered_data.csv')
-
-        # Preprocessing
-        self.data['DateTime'] = pd.to_datetime(self.data['DateTime'])
-        self.data['HourOfDay'] = self.data['DateTime'].dt.hour
-        self.data['DayOfWeek'] = self.data['DateTime'].dt.dayofweek
-        self.data['Weekend'] = self.data['DayType'].apply(lambda x: 1 if x == 'Weekend' else 0)
-
-        # Encoding categorical variables
-        self.enc = OneHotEncoder(drop='first')
-        encoded_features = self.enc.fit_transform(self.data[['HourOfDay', 'DayOfWeek', 'Weekend']]).toarray()
-        self.encoded_cols = self.enc.get_feature_names_out(['HourOfDay', 'DayOfWeek', 'Weekend'])
-        self.data[self.encoded_cols] = encoded_features
-
-        # Model training
-        self.rf = RandomForestClassifier(n_estimators=100, random_state=42)
-        X = self.data.drop(['Items', 'DateTime', 'DayType', 'HourOfDay', 'Weekend'], axis=1)
+        self.features = ['Time', 'DayPart', 'DayType']  # Define relevant features
+        self.label_encoder = LabelEncoder()
+        self.model = self.train_model()
+    def preprocess_data(self):
+        # Encode categorical variables
+        for feature in self.features:
+            self.data[feature] = self.label_encoder.fit_transform(self.data[feature])
+    def train_model(self):
+        self.preprocess_data()
+        X = self.data[self.features]
         y = self.data['Items']
-        self.rf.fit(X, y)
-
-    def predict_food_item(self, data):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = LogisticRegression()
+        model.fit(X_train, y_train)
+        return model
+    def predict_food(self, data):
         try:
-            time_of_day = data['TimeOfDay']  # Update key to match frontend
-            day_of_week = data['DayOfWeek']  # Update key to match frontend
-            time = data['Time']
-
-            # Encoding input data
-            encoded_data = self.enc.transform([[time_of_day, day_of_week, 1 if day_of_week in [5, 6] else 0]]).toarray()
-            input_features = np.hstack((time, encoded_data))
-
-            # Predicting food item
-            predicted_item = self.rf.predict([input_features])[0]
-            return {'PredictedFoodItem': predicted_item}
+            # Encode input data
+            encoded_data = [self.label_encoder.transform([data[feature]])[0] for feature in self.features]
+            # Predict using the trained model
+            food_prediction = self.model.predict([encoded_data])[0]
+            return {'food_prediction': food_prediction}
         except Exception as e:
             return {'error': str(e)}
-        
     def post(self):
         try:
             data = request.json
-            result = self.predict_food_item(data)
+            result = self.predict_food(data)
             return jsonify(result)
         except Exception as e:
             return jsonify({'error': str(e)})
-        
-api.add_resource(FoodAPI, '/food')
+
+api.add_resource(FoodPredictor, '/predict')
